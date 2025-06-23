2bd84f82722ab5d68802622aaa9dbfc931ec4fb1
Jayesh Jadhav <krishnajadhav9632@gmail.com>
2025-06-23 15:52:21 +0530

    Removed Bugs

--- src/main/java/com/Jayesh/SplitApp/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.Jayesh.SplitApp.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-    // Add beans or CORS config if needed in future
-}
