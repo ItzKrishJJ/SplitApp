2a44b3eb87afb6827f6aa07e29de8a0d4f926bd9
Jayesh Jadhav <krishnajadhav9632@gmail.com>
2025-06-23 16:02:15 +0530

    Removed Bugs

--- src/main/java/com/Jayesh/SplitApp/controller/PersonController.java
+++ src/main/java/com/Jayesh/SplitApp/controller/PersonController.java
@@ -7,11 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+@CrossOrigin
 @Controller
 @RequestMapping("/people")
 @RequiredArgsConstructor
